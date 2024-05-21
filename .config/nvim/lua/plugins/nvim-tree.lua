a108c03623fbe7c3d16b6362d402e6486bb153b4
flieris <sebastian.lenkiewicz94@gmail.com>
2024-05-21 20:05:04 +0200

    fixed nvim-tree config

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,11 +1,11 @@
-local nvim_tree = require("nvim-tree")
-
-nvim_tree.setup({
-  auto_reload_on_write = true,
-  disable_netrw = false,
-  hijack_netrw = true,
-  hijack_cursor = false,
-  open_on_tab = false,
-  sort_by = "name",
-  update_cwd = false,
-})
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependences = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup({})
+  end,
+}
