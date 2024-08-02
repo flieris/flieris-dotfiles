f7fe164be5d94bcb9bcf2977498256eed7cffa87
flieris <sebastian.lenkiewicz94@gmail.com>
2024-08-02 21:32:37 +0200

    Changed: keymaps configs and toggleterm config
    Also added todo to sync config files

---
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    config = true,
+    cmd = "ToggleTerm",
+    keys = {
+      { "<F4>", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Open Terminal" },
+    },
+  },
+}
