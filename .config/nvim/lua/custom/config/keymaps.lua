1223673b9e86fb9818cb48a2fd035bf4bac91d15
flieris <sebastian.lenkiewicz94@gmail.com>
2025-08-12 20:14:12 +0200

    added noice and keymaps for nvim-dap

--- .config/nvim/lua/custom/config/keymaps.lua
+++ .config/nvim/lua/custom/config/keymaps.lua
@@ -29,7 +29,8 @@ vim.keymap.set('n', '<C-Right>', '<C-w>l', { desc = 'Go to Right window', remap 
 vim.keymap.set('n', '<S-Left>', '<cmd>bprevious<cr>', { desc = 'Go to previous tab' })
 vim.keymap.set('n', '<S-Right>', '<cmd>bnext<cr>', { desc = 'Go to next tab' })
 vim.keymap.set('n', '<leader>bd', function()
-  Snacks.bufdelete()
+
+Snacks.bufdelete()
 end, { desc = 'Delete buffer ' })
 vim.keymap.set('n', '<C-/>', '<cmd>ToggleTerm<cr>', { desc = 'Open terminal' })
 
@@ -40,3 +41,21 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.keymap.set('n', "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, { desc = "Breakpoint Condition" })
+vim.keymap.set('n', "<leader>db", function() require("dap").toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
+vim.keymap.set('n', "<leader>dc", function() require("dap").continue() end, { desc = "Run/Continue" })
+vim.keymap.set('n', "<leader>da", function() require("dap").continue({ before = get_args }) end, { desc = "Run with Args" })
+vim.keymap.set('n', "<leader>dC", function() require("dap").run_to_cursor() end, { desc = "Run to Cursor" })
+vim.keymap.set('n', "<leader>dg", function() require("dap").goto_() end, { desc = "Go to Line (No Execute)" })
+vim.keymap.set('n', "<leader>di", function() require("dap").step_into() end, { desc = "Step Into" })
+vim.keymap.set('n', "<leader>dj", function() require("dap").down() end, { desc = "Down" })
+vim.keymap.set('n', "<leader>dk", function() require("dap").up() end, { desc = "Up" })
+vim.keymap.set('n', "<leader>dl", function() require("dap").run_last() end, { desc = "Run Last" })
+vim.keymap.set('n', "<leader>do", function() require("dap").step_out() end, { desc = "Step Out" })
+vim.keymap.set('n', "<leader>dO", function() require("dap").step_over() end, { desc = "Step Over" })
+vim.keymap.set('n', "<leader>dP", function() require("dap").pause() end, { desc = "Pause" })
+vim.keymap.set('n', "<leader>dr", function() require("dap").repl.toggle() end, { desc = "Toggle REPL" })
+vim.keymap.set('n', "<leader>ds", function() require("dap").session() end, { desc = "Session" })
+vim.keymap.set('n', "<leader>dt", function() require("dap").terminate() end, { desc = "Terminate" })
+vim.keymap.set('n', "<leader>dw", function() require("dap.ui.widgets").hover() end, { desc = "Widgets" })
